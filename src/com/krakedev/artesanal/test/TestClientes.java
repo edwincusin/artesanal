ed4d611ccad0d02a28721a84cb9ebb360bf95bc2
EDWINCSN <c.edwin32@hotmail.com>
2026-04-26 03:26:45 -0500

    EGCA: se agrega la clase TestCliente main, para probar el nullPointerExcepcion y correccion errores

---
+++ src/com/krakedev/artesanal/test/TestClientes.java
@@ -0,0 +1,13 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.NegocioMejorado;
+
+public class TestClientes {
+
+	public static void main(String[] args) {
+		NegocioMejorado nm= new NegocioMejorado();
+		nm.registrarCliente("Edwin", "1753081056");
+
+	}
+
+}
